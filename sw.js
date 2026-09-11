d14a47590c47f067ef3df52abddad99816d6a9a7
zhengyayu <zhengyayu@zjwhyis.com>
2026-09-11 16:13:27 +0800

    add data export/import

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // 记一笔 PWA Service Worker
-const CACHE_NAME = 'jybook-v1';
-const ASSETS = ['./', './code.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE_NAME = 'jybook-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
@@ -25,7 +25,7 @@ self.addEventListener('fetch', e => {
           caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
         }
         return res;
-      }).catch(() => caches.match('./code.html'));
+      }).catch(() => caches.match('./index.html'));
     })
   );
 });
